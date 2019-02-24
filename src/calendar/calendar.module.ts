96a5ad09a7881d3088f6f824b8005e56020d71d6
hdahash <hdahash@egmail.com>
2019-02-24 22:39:12 +0300

    change dependencies to @ionic/angular

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,21 +1,11 @@
-import { NgModule } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
-import { Calendar } from './calendar';
-import { monthName } from './pipes/month-name';
-import {weekdayName} from "./pipes/weekday-name";
+import { NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+import { Calendar } from "./calendar";
+import { monthName } from "./pipes/month-name";
+import { weekdayName } from "./pipes/weekday-name";
 @NgModule({
-  declarations: [
-    Calendar,
-    monthName,
-    weekdayName
-  ],
-  imports: [
-    IonicModule,
-  ],
-  exports: [
-    Calendar,
-    monthName,
-    weekdayName
-  ]
+  declarations: [Calendar, monthName, weekdayName],
+  imports: [IonicModule],
+  exports: [Calendar, monthName, weekdayName]
 })
-export class CalendarModule { }
+export class CalendarModule {}
